7fb8123d9d186fe49caece0da32aa7da28602660
Metel Patel <metelpatel@gmail.com>
2016-01-29 11:22:19 -0600

    pushing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Dog = require('../models/dog')
+
+function randomDog(dogsArray){
+  var dogs = dogsArray;
+  return dogs[Math.floor(Math.random()*dogs.length)];
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  Dog.find({}, 'dog', function(err,data){
+    if (err) console.log(err);
+
+    var dog = randomDog(data);
+  })
+
+
+  res.render('index', { title: 'OKC', dog: dog.dog });
 });
 
 module.exports = router;
